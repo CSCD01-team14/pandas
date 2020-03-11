f8332acbe8e19171c1c6df8ece952f78e365e389
Will Song <16807975+WillySong@users.noreply.github.com>
2020-03-11 01:50:32 -0400

    more test cases added
    six of hopeful 8. Covered all I can think of. empty doesn't work

--- pandas/core/groupby/_test_get_groups.py
+++ pandas/core/groupby/_test_get_groups.py
@@ -1,37 +1,12 @@
+# DO NOT MERGE _test_get_groups.py WITH ACTUAL PANDAS 
+# DO NOT MERGE _test_get_groups.py WITH ACTUAL PANDAS 
+# DO NOT MERGE _test_get_groups.py WITH ACTUAL PANDAS 
+
 import unittest
 
 from pandas import DataFrame
 from pandas.testing import assert_frame_equal
 import numpy as np
-
-#
-# # df = DataFrame({'Animal': ['Falcon', 'Falcon',
-# #                               'Parrot', 'Parrot'],
-# #                    'Max Speed': [380., 370., 24., 26.],
-# #                 'bullshit': ['a', 'b', 'c', 'd']})
-# #
-# # print(df)
-# # grps = df.groupby(['Animal'])
-# # print(_GroupBy.get_group(grps, ('Falcon')))
-# # print("=====")
-# # i = 0
-# # for key, item in grps:
-# #     print(i)
-# #     i +=1
-# #     print(key)
-# #     print(_GroupBy.get_group(grps, key))
-# # print(grps)
-# # print(grps.mean())
-# # print("=====")
-#
-# d = {
-#             "cat": pd.Categorical(
-#                 ["a", "b", "a", "b"], categories=["a", "b", "c"], ordered=True
-#             ),
-#             "ints": [1, 1, 2, 2],
-#             "val": [10, 20, 30, 40],
-#         }
-#         df = pd.DataFrame(d)
 
 class _test_get_groups(unittest.TestCase):
     def test_cero_discovered_invalid(self):
@@ -79,6 +54,27 @@ class _test_get_groups(unittest.TestCase):
         })
         grps = df.groupby(by=['B'])
         self.assertEqual(True, np.array_equal(grps.get_group(('b1')).values, expected.values))
+
+    def test_cuatro_expect_empty(self):
+        df = DataFrame(data={
+            'A': ['a1', 'a2', None],
+            'B': ['b1', 'b2', 'b1'],
+            'val': [1, 2, 3],
+        })
+        grps = df.groupby(by=['B'])
+        self.assertRaises(KeyError, grps.get_group, ('b3'))
+
+    def test_cinco_double_row_valid(self):
+        df = DataFrame({'Animal': ['Falcon', 'Falcon',
+                                   'Parrot', 'Parrot'],
+                        'Max Speed': [380., 370., 24., 26.],
+                        'bullshit': ['a', 'b', 'c', 'd']})
+        expected = DataFrame({'Animal': ['Falcon', 'Falcon'],
+                        'Max Speed': [380., 370.],
+                        'bullshit': ['a', 'b']})
+        grps = df.groupby(['Animal'])
+        assert_frame_equal(grps.get_group(('Falcon')), expected)
+
 
 
 if __name__ == '__main__':
