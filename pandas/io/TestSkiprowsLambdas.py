87266f44a18494d6d2ffba363603186399dd3cae
Will Song <16807975+WillySong@users.noreply.github.com>
2020-03-20 12:35:10 -0400

    Added basic test suite

---
+++ pandas/io/TestSkiprowsLambdas.py
@@ -0,0 +1,110 @@
+import unittest
+import pandas as pd
+from pandas import DataFrame
+from pandas.testing import assert_frame_equal
+
+
+class TestSkiprowsLambdas(unittest.TestCase):
+    def test_single_column_equal(self):
+        schema = {
+            "show_id": int,
+            "type": String,
+            "title": String,
+            "director": String,
+            "cast": String,
+            "country": String,
+            "date_added": String,
+            "release_year": int,
+            "rating": String,
+            "duration": String,
+            "listed_in": String,
+            "description": String
+        }
+        mask = (show_id == 70153404)
+        df = pd.read_csv('netflix_titles.csv', skiprows=mask, dtype=schema)
+        expected = DataFrame(data={
+            "show_id": [70153404],
+            "type": ["TV Show"],
+            "title": [],
+            "director": [],
+            "cast": ["Jennifer Aniston, Courteney Cox, Lisa Kudrow, Matt LeBlanc, Matthew Perry, David Schwimmer"],
+            "country": ["United States"],
+            "date_added": [],
+            "release_year": [2003],
+            "rating": ["TV - 14"],
+            "duration": ["10 Seasons"],
+            "listed_in": ["Classic & Cult TV, TV Comedies"],
+            "description": ["This hit sitcom follows the merry misadventures of six 20-something pals as they navigate the pitfalls of work, life and love in 1990s Manhattan."]
+        })
+        assert_frame_equal(df, expected)
+
+    def test_single_column_less_than(self):
+        schema = {
+            "country": String,
+            "capital": String,
+            "area": int,
+            "population": int
+        }
+        mask = (area > 8.516)
+        df = pd.read_csv('brics.csv', skiprows=mask, dtype=schema)
+        expected = DataFrame(data={
+            "country": ["Russia", "China"],
+            "capital": ["Moscow", "Beijing"],
+            "area": [17.10, 9.597],
+            "population": [143.5, 1357]
+        })
+        assert_frame_equal(df, expected)
+
+    def test_single_column_greater_than(self):
+        schema = {
+            "country": String,
+            "capital": String,
+            "area": int,
+            "population": int
+        }
+        mask = (area < 8.516)
+        df = pd.read_csv('brics.csv', skiprows=mask, dtype=schema)
+        expected = DataFrame(data={
+            "country": ["India", "South Africa"],
+            "capital": ["New Dehli", "Pretoria"],
+            "area": [3.286, 1.221],
+            "population": [1252, 52.98]
+        })
+        assert_frame_equal(df, expected)
+
+    def test_multi_columns_and(self):
+        schema = {
+            "country": String,
+            "capital": String,
+            "area": int,
+            "population": int
+        }
+        mask = (area <= 8.516 and population > 1200)
+        df = pd.read_csv('brics.csv', skiprows=mask, dtype=schema)
+        expected = DataFrame(data={
+            "country": ["India", "China"],
+            "capital": ["New Dehli", "Beijing"],
+            "area": [3.286, 9.597],
+            "population": [1252, 1357]
+        })
+        assert_frame_equal(df, expected)
+
+    def test_multi_columns_or(self):
+        schema = {
+            "country": String,
+            "capital": String,
+            "area": int,
+            "population": int
+        }
+        mask = (area > 8.516 or area < 3.286)
+        df = pd.read_csv('brics.csv', skiprows=mask, dtype=schema)
+        expected = DataFrame(data={
+            "country": ["Russia", "South Africa"],
+            "capital": ["Moscow", "Pretoria"],
+            "area": [17.10, 1.221],
+            "population": [143.5, 52.98]
+        })
+        assert_frame_equal(df, expected)
+
+if __name__ == '__main__':
+    unittest.main()
